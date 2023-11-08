764a322a044b594731afe5edccaedddf103c8548
sambos <salaam@samboss-mbp.lan>
2023-11-08 17:00:21 -0600

    updated

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  cloud {
+    organization = "rsol-prod"
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
