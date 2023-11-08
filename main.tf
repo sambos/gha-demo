bb7f453deb26b1bbef58e085dff97c819728028a
sambos <salaam@samboss-mbp.lan>
2023-11-08 17:05:05 -0600

    updated

--- main.tf
+++ main.tf
@@ -5,10 +5,11 @@ terraform {
       name = "learn-terraform-cloud"
     }
   }
+}
 
 # An example resource that does nothing.
 resource "null_resource" "example" {
   triggers = {
     value = "A example resource that does nothing!"
   }
-}
+
