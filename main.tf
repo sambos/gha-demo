3a53d34a4f9d51e9cb49e18ac60c1c3a9603df27
sambos <salaam@samboss-mbp.lan>
2023-11-08 17:28:52 -0600

    updated

--- main.tf
+++ main.tf
@@ -7,9 +7,17 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::995241849584:role/rsol-iaac"
+  }
+}
+
 # An example resource that does nothing.
 resource "null_resource" "example" {
   triggers = {
     value = "A example resource that does nothing!"
   }
-
+}
